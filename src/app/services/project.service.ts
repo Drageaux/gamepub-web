99d1b4c0ccd1e756d9aa2cb6a451ade18c26a2aa
David Thong Nguyen <dev@davidtn.com>
2021-10-02 23:02:41 -0400

    Project builder placeholder function

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,18 +1,41 @@
+import { Package } from './../classes/package';
+import { UnityManifest } from './../classes/unity-manifest';
+import { Project } from './../classes/project';
 import { Injectable } from '@angular/core';
-import { UnityManifest } from '../classes/unity-manifest';
-import testData from './test-data/manifest.json';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
-  data: UnityManifest = testData;
-  constructor() {
-    console.log(this.data);
-    this.parseManifest(this.data);
+  // data = testData;
+  constructor(private http: HttpClient) {
+    // console.log(this.data.dependencies);
+    // this.parseManifest(this.data);
+    this.getManifest();
   }
 
   // TODO: parse project structure
+  projectBuilder(name: string = 'New Project'): Project {
+    const proj: Project = {
+      name,
+    };
+    return proj;
+  }
 
-  parseManifest(manifest: UnityManifest) {}
+  parseManifest(manifest: UnityManifest) {
+    let unityRegistryPkgs = manifest.dependencies;
+    console.log(unityRegistryPkgs);
+    return;
+  }
+
+  getManifest() {
+    this.http
+      .get<UnityManifest>('./assets/test-data/manifest.json')
+      .subscribe((res) => {
+        for (const [k, v] of Object.entries(res.dependencies)) {
+          console.log(k, v);
+        }
+      });
+  }
 }
