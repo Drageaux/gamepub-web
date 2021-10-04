eaad311cd17c0203a24a869084d22a504a41e9d3
David Thong Nguyen <dev@davidtn.com>
2021-10-03 23:55:20 -0400

    Generated GitHub object interfaces

---
+++ src/app/classes/github-contents.ts
@@ -0,0 +1,19 @@
+// generated using https://jvilk.com/MakeTypes/
+export interface GithubContents {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  _links: GithubLinks;
+}
+
+export interface GithubLinks {
+  self: string;
+  git: string;
+  html: string;
+}
