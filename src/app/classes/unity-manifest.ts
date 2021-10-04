77d45400800f5a66ba8939f05b7c518e8036214d
David Thong Nguyen <dev@davidtn.com>
2021-10-04 00:25:47 -0400

    Update unity-manifest.ts

--- src/app/classes/unity-manifest.ts
+++ src/app/classes/unity-manifest.ts
@@ -1,12 +1,12 @@
 import { PackageName, VersionName } from './package';
 
+export interface UnityManifest {
+  dependencies: Record<PackageName, VersionName>; // "com.unity."
+  scopedRegistries: Registry[];
+}
+
 export interface Registry {
   name: string;
   url: string;
   scopes: string[];
-}
-
-export interface UnityManifest {
-  dependencies: Record<PackageName, VersionName>; // "com.unity."
-  scopedRegistries: Registry[];
 }
