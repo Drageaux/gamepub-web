c93e0296087835679fe1baaa77f24ac31e551db1
David Thong Nguyen <dev@davidtn.com>
2021-10-04 01:13:56 -0400

    Decode the content of file type right after HTTP

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -5,7 +5,7 @@ import { UnityManifest } from './../classes/unity-manifest';
 import { Project } from './../classes/project';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, mergeMap, map } from 'rxjs/operators';
+import { switchMap, mergeMap, map, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 @Injectable({
@@ -36,15 +36,26 @@ export class ProjectService {
     // } else return throwError('Cannot find default branch of project!');
     let unityRegistryPkgs = manifest.dependencies;
     console.log(unityRegistryPkgs);
-    return;
+    return unityRegistryPkgs;
   }
 
-  getManifest(owner: string, repo: string) {
-    // NOTE: Packages or UnityPackageManager folder
+  getManifest(owner: string, repo: string, path: string = 'Packages') {
     // TODO: Sometimes the file may be in a different location, so we should support custom path later
-    return this.http.get<UnityManifest>(
-      `https://api.github.com/repos/${owner}/${repo}/contents/Packages/manifest.json`
-    );
+    // NOTE: Packages or UnityPackageManager folder
+    return this.http
+      .get<GithubContents>(
+        `https://api.github.com/repos/${owner}/${repo}/contents/${path}/manifest.json`
+      )
+      .pipe(
+        map((res) => {
+          // file type from this API endpoint has base64 encoded content
+          if (res.type === 'file' && res.content) {
+            const decoded = atob(res.content);
+            return JSON.parse(decoded);
+          } else return throwError('Not a valid file');
+        })
+      );
+    // delete below for test on custom file
     // return this.http.get<UnityManifest>('./assets/test-data/manifest.json');
     // .subscribe((res) => {
     //   for (const [k, v] of Object.entries(res.dependencies)) {
