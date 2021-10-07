4f5d5b7096aca56d143e042b632d8837f8def25f
David Thong Nguyen <dev@davidtn.com>
2021-10-07 01:19:31 -0400

    Getting info of each openupm package

--- src/app/services/package.service.ts
+++ src/app/services/package.service.ts
@@ -1,7 +1,8 @@
+import { RegistryScope } from './../classes/scoped-registry';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { forkJoin, Observable } from 'rxjs';
+import { map, shareReplay, take, tap } from 'rxjs/operators';
 import { Package } from '../classes/package';
 
 type PackageName = string;
@@ -10,16 +11,25 @@ type PackageName = string;
   providedIn: 'root',
 })
 export class PackageService {
-  apiUrl = 'https://api.openupm.com';
+  // openUpmApiUrl = 'https://api.openupm.com/packages';
+  // openUpmPackagesUrl = 'https://package.openupm.com';
+  openUpmPackagesUrl = '/registry/openupm';
   allPackagesUrl = '/packages/extra';
 
   constructor(private http: HttpClient) {}
 
-  getAll() {
-    return this.http
-      .get<Map<PackageName, Package>>(`${this.apiUrl}${this.allPackagesUrl}`)
-      .pipe(map((val) => val));
-  }
+  // getAll() {
+  //   return this.http
+  //     .get<Map<PackageName, Package>>(
+  //       `${this.openUpmApiUrl}${this.allPackagesUrl}`
+  //     )
+  //     .pipe(map((val) => val));
+  // }
 
-  // TODO: parse manifest file
+  expandOpenUpmPackageInfo(scope: RegistryScope): Observable<Package> {
+    console.log(scope);
+    return this.http
+      .get<Package>(`${this.openUpmPackagesUrl}/${scope}`)
+      .pipe(tap(console.log), shareReplay());
+  }
 }
