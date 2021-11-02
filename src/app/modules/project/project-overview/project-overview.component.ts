116df0fc334ba99f7122cf9ed654dd0826fa9021
David Thong Nguyen <dev@davidtn.com>
2021-11-02 00:12:27 -0400

    Adding ProjectOverview component

---
+++ src/app/modules/project/project-overview/project-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-overview',
+  templateUrl: './project-overview.component.html',
+  styleUrls: ['./project-overview.component.scss']
+})
+export class ProjectOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
