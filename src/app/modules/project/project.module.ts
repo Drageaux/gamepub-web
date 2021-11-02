30322ff5fb47b38232cc36c83c1da0d88ba9a2d1
David Thong Nguyen <dev@davidtn.com>
2021-11-02 00:13:45 -0400

    Enabling tabs in the UI

--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -4,18 +4,19 @@ import { ProjectComponent } from './project.component';
 import { OpenupmPackageDetailsComponent } from './openupm-package-details/openupm-package-details.component';
 import { PackageListingComponent } from './package-listing/package-listing.component';
 import { ProjectTreeComponent } from './project-tree/project-tree.component';
-
-
+import { ProjectOverviewComponent } from './project-overview/project-overview.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     ProjectComponent,
     OpenupmPackageDetailsComponent,
     PackageListingComponent,
-    ProjectTreeComponent
+    ProjectTreeComponent,
+    ProjectOverviewComponent,
+    ProjectDetailsComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
 })
-export class ProjectModule { }
+export class ProjectModule {}
