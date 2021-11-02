d1ae5c086a54eeebba1538c0334e3f0a70f7f8db
David Thong Nguyen <dev@davidtn.com>
2021-11-02 00:11:56 -0400

    Splitting up Project Overview and Details tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AssetDashboardComponent } from '@modules/asset-dashboard/asset-dashboard.component';
+import { ProjectDetailsComponent } from '@modules/project/project-details/project-details.component';
+import { ProjectOverviewComponent } from '@modules/project/project-overview/project-overview.component';
 import { ProjectComponent } from '@modules/project/project.component';
 
 const routes: Routes = [
-  { path: 'project/:id', component: ProjectComponent },
+  {
+    path: 'project/:id',
+    component: ProjectComponent,
+    children: [
+      {
+        path: '',
+        component: ProjectOverviewComponent,
+      },
+      {
+        path: 'details',
+        component: ProjectDetailsComponent,
+      },
+    ],
+  },
   { path: 'dashboard', component: AssetDashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // redirect
   // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
