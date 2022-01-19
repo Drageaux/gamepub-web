e88a75d4a3096f7e589d2bb5890335beba9a9612
David Thong Nguyen <dev@davidtn.com>
2022-01-19 17:59:07 +0700

    🏗️ No providedIn in package service to prevent cyclic dependency error

--- src/app/services/package.service.ts
+++ src/app/services/package.service.ts
@@ -6,9 +6,7 @@ import { Package } from '@classes/package';
 import { RegistryScope } from '@classes/scoped-registry';
 import { ProjectModule } from '@modules/project/project.module';
 
-@Injectable({
-  providedIn: ProjectModule,
-})
+@Injectable()
 export class PackageService {
   // openUpmApiUrl = 'https://api.openupm.com/packages';
   // openUpmPackagesUrl = 'https://package.openupm.com';
