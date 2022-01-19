9d200d225b853c694c191dcd5c7859b2e1ea69a6
David Thong Nguyen <dev@davidtn.com>
2022-01-19 21:17:54 +0700

    ✨ Rendering raw user's projects JSON in profile

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '@classes/project';
 import { User } from '@classes/user';
 import { ProjectService } from '@services/project.service';
 import { UserService } from '@services/shared/user.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -11,9 +13,19 @@ import { Observable } from 'rxjs';
 })
 export class ProfileComponent implements OnInit {
   profile$!: Observable<User>;
-  constructor(private userService: UserService) {}
+  projects$!: Observable<Project[]>;
+
+  constructor(
+    private userService: UserService,
+    private projectService: ProjectService
+  ) {}
 
   ngOnInit(): void {
     this.profile$ = this.userService.profile$;
+    this.projects$ = this.profile$.pipe(
+      switchMap((prof: User) =>
+        prof ? this.projectService.getProjectsByUsername(prof.username) : of([])
+      )
+    );
   }
 }
