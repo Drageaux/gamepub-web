92c0cef0966396d379781099747a5ebc436fdeda
David Thong Nguyen <dev@davidtn.com>
2022-01-20 10:45:45 +0700

    Update profile.module.ts

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class ProfileModule {}
