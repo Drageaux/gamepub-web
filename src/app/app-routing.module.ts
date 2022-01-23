b1185643fd7fcb14952aae02a63879ea529e16d9
David Thong Nguyen <dev@davidtn.com>
2022-01-23 15:24:13 +0700

    🚧 Wiring component to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './modules/profile/profile.component';
 import { AssetDashboardComponent } from '@modules/asset-dashboard/asset-dashboard.component';
-import { ProjectComponent } from '@modules/project/project.component';
-import { CreateProjectComponent } from '@modules/project/create-project/create-project.component';
 import { CreateAssetComponent } from '@modules/asset/create-asset/create-asset.component';
+import { CreateProjectComponent } from '@modules/project/create-project/create-project.component';
+import { ProjectProxyComponent } from '@modules/project/project-proxy/project-proxy.component';
+import { ProjectComponent } from '@modules/project/project.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 const routes: Routes = [
   { path: 'new-project', component: CreateProjectComponent },
   { path: 'new-asset', component: CreateAssetComponent },
   { path: 'dashboard', component: AssetDashboardComponent },
+  { path: 'project/:id', component: ProjectProxyComponent },
   {
     path: ':username',
     children: [
