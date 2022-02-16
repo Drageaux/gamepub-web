e3029acc75f8cb7b28bcc3d2977bdd733b15afc6
David Thong Nguyen <dev@davidtn.com>
2022-02-16 15:30:44 -0500

    💄 UI for new job form

--- src/app/modules/project/create-job/create-job.component.ts
+++ src/app/modules/project/create-job/create-job.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { JobApiService } from '@services/job-api.service';
 
 @Component({
   selector: 'app-create-job',
   templateUrl: './create-job.component.html',
-  styleUrls: ['./create-job.component.scss']
+  styleUrls: ['./create-job.component.scss'],
 })
 export class CreateJobComponent implements OnInit {
+  jobForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(100),
+    ]),
+    body: new FormControl(''),
+  });
 
-  constructor() { }
+  @ViewChild('form') form!: NgForm;
 
-  ngOnInit(): void {
+  constructor(private jobApi: JobApiService) {}
+
+  ngOnInit(): void {}
+
+  get title() {
+    return this.jobForm.get('title');
   }
 
+  get body() {
+    return this.jobForm.get('body');
+  }
+
+  onSubmit() {}
 }
