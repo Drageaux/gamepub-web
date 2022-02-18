01afaea40af3bae85b98341d1acb308bcc34abbe
David Thong Nguyen <dev@davidtn.com>
2022-02-18 16:44:20 -0500

    🏷️ Update job comment class

--- src/app/classes/job-comment.ts
+++ src/app/classes/job-comment.ts
@@ -1,8 +1,13 @@
+import { Job } from './job';
 import { Project } from './project';
+import { User } from './user';
 
 export interface JobComment {
   _id?: string;
-  text: string;
+  user?: string | User;
+  project: string | Project;
+  job: string | Job;
+  body: string;
   createdAt?: string | Date;
   updatedAt?: string | Date;
 }
