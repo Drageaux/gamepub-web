66e2baa33c4976ce249661abcb6dc4e7dae1dacc
David Thong Nguyen <dev@davidtn.com>
2022-02-22 15:36:39 -0500

    ➕ Using RouterModule in jobs

--- src/app/modules/jobs/jobs.module.ts
+++ src/app/modules/jobs/jobs.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobsComponent } from './jobs.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [JobsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class JobsModule {}
