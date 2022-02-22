6e813632ab3afa05118a87a997742d9bc472113a
David Thong Nguyen <dev@davidtn.com>
2022-02-22 16:41:09 -0500

    📱 New image and responsive design added

--- src/app/modules/jobs/jobs.module.ts
+++ src/app/modules/jobs/jobs.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobsComponent } from './jobs.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '@modules/shared/shared.module';
 
 @NgModule({
   declarations: [JobsComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class JobsModule {}
