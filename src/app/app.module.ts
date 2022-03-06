5f9c44de8795bb7a1505c42d335f32751cf32187
David Thong Nguyen <dev@davidtn.com>
2022-03-06 00:46:35 -0500

    🛂 Adding auth bearer to requests to node API endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,6 +56,9 @@ import { environment } from 'src/environments/environment';
               scope: 'read:current_user',
             },
           },
+          {
+            uri: `${environment.apiUrl}/*`,
+          },
         ],
       },
     }),
