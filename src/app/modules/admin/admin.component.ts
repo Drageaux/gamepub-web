f017f316b4bf40ec98be4136bf80652ab9b01e17
David Thong Nguyen <dev@davidtn.com>
2022-03-09 19:05:01 -0500

    ⚡ Avoid memory leak when navigating off this page

--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Project } from '@classes/project';
 import { AdminService } from '@services/admin.service';
 import { ProjectsApiService } from '@services/projects-api.service';
@@ -6,25 +6,26 @@ import { UsersApiService } from '@services/users-api.service';
 import { UsersService } from '@services/users.service';
 import { of } from 'rxjs';
 import { catchError, map, switchMap, filter, toArray } from 'rxjs/operators';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
   games!: Project[];
   constructor(
     private adminService: AdminService,
     private usersApi: UsersApiService
   ) {}
-
   ngOnInit(): void {
     this.games = this.adminService.parseSteamStore();
   }
 
   generateProjects() {
-    this.adminService
+    this.subs.sink = this.adminService
       .createNewTestUsers(this.games)
       .pipe(
         switchMap((res) => {
@@ -37,5 +38,9 @@ export class AdminComponent implements OnInit {
         })
       )
       .subscribe(console.log, console.error);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
