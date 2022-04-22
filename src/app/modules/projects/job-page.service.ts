2892076cf530a1067aaa71d7d8dde83f5a0d6a95
David Thong Nguyen <dev@davidtn.com>
2022-04-22 18:03:12 -0400

    🏗️ Restructure the submissions to broadcast changes

--- src/app/modules/projects/job-page.service.ts
+++ src/app/modules/projects/job-page.service.ts
@@ -3,7 +3,7 @@ import { SubSink } from 'subsink';
 import { Injectable, OnDestroy } from '@angular/core';
 import { Project } from '@classes/project';
 import { ProjectsApiService } from '@services/projects-api.service';
-import { ReplaySubject, Observable, pipe } from 'rxjs';
+import { ReplaySubject, Observable, pipe, BehaviorSubject, of } from 'rxjs';
 import { Job } from '@classes/job';
 import { JobsApiService } from '@services/jobs-api.service';
 import { ProjectsModule } from './projects.module';
@@ -24,7 +24,7 @@ export class JobPageService implements OnDestroy {
 
   private _job$ = new ReplaySubject<Job | null>(1);
   private _jobNumber = 0;
-  private _submissions$ = new ReplaySubject<JobSubmission[]>(1);
+  private _submissions$ = new BehaviorSubject<JobSubmission[]>([]);
 
   constructor(private jobsApi: JobsApiService) {}
 
@@ -66,11 +66,33 @@ export class JobPageService implements OnDestroy {
     return this._submissions$.asObservable();
   }
 
+  postSubmission(
+    creator: string,
+    projectName: string,
+    jobNumber: number,
+    input: JobSubmission
+  ) {
+    const { githubRepo, body } = input;
+
+    return this.jobsApi
+      .postJobSubmission(creator, projectName, jobNumber, {
+        githubRepo: githubRepo.trim(),
+        body: body?.trim(),
+      } as JobSubmission)
+      .pipe(
+        tap((newSubmission) => {
+          const newArr = [...this._submissions$.value, newSubmission];
+          this._submissions$.next(newArr);
+          return newSubmission;
+        })
+      );
+  }
+
   resetJob(): void {
     this._job$.complete();
     this._job$ = new ReplaySubject<Job | null>(1);
     this._submissions$.complete();
-    this._submissions$ = new ReplaySubject<JobSubmission[]>(1);
+    this._submissions$ = new BehaviorSubject<JobSubmission[]>([]);
     this._jobNumber = 0;
   }
 
